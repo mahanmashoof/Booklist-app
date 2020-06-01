aad76bb94e7bef5b93d76dbad5db7f4917e49dee
Mahan Mashoof <mahan.mashoof@gmail.com>
2020-06-01 18:47:40 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//Representation of a Book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+//Handling UI tasks
+class UI {
+  static displayBooks() {
+    
+  }
+}
