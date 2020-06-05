bbcbfac4d6732003c7356d229918c28123a2776f
Mahan Mashoof <mahan.mashoof@gmail.com>
2020-06-05 19:31:24 -0300

    validation ok

--- app.js
+++ app.js
@@ -11,22 +11,22 @@ class Book {
 
 //Handling UI tasks
 class UI {
-  static displayBooks() {
-    const StoredBooks = [
-      {
-        title: 'Book 1',
-        author: 'author 1',
-        isbn: '1111'
-      },
-      {
-        title: 'Book 2',
-        author: 'author 2',
-        isbn: '2222'
-      }
-    ];
-    const books = StoredBooks;
-    books.forEach((book) => UI.addBookToList(book));
-  }
+  // static displayBooks() {
+  //   const StoredBooks = [
+  //     {
+  //       title: 'Book 1',
+  //       author: 'author 1',
+  //       isbn: '1111'
+  //     },
+  //     {
+  //       title: 'Book 2',
+  //       author: 'author 2',
+  //       isbn: '2222'
+  //     }
+  //   ];
+  //   const books = StoredBooks;
+  //   books.forEach((book) => UI.addBookToList(book));
+  // }
   static addBookToList(book) {
     const list = document.querySelector('#book-list');
     const row = document.createElement('tr');
@@ -34,7 +34,7 @@ class UI {
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.isbn}</td>
-      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+      <td><a href="#" class="btn btn-warning btn-sm delete">X</a></td>
     `;
     list.appendChild(row);
   }
@@ -50,6 +50,18 @@ class UI {
       element.parentElement.parentElement.remove();
     }
   }
+
+  //Bootstrap validation methos built in JS
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+    //Make dissapear in x seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 2000);
+  }
 }
 
 //Display Books
@@ -60,15 +72,37 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const isbn = document.querySelector('#isbn').value;
-  //Instatiate book
-  const book = new Book(title, author, isbn);
-  //Add book to UI
-  UI.addBookToList(book);
-  //Clear fields
-  UI.clearFields();
+  //Validate
+  if (title === '' || author === '' || isbn === '') {
+    UI.showAlert("one filed is empty", "danger");
+  } else {
+    //Instatiate book
+    const booka = new Book(title, author, isbn);
+    //Add book to UI
+    UI.addBookToList(booka);
+    //Show success message
+    UI.showAlert('Book added!', 'success');
+    //Clear fields
+    UI.clearFields();
+  }
 });
 
 //Remove a book (event propagation: target the whole list and delete the grandparent)
 document.querySelector('#book-list').addEventListener('click', (e) =>{
   UI.deleteBook(e.target);
+  //Show deleted message
+  UI.showAlert('Book deleted!', 'warning');
 });
+
+//Local Storage
+class Store {
+  static getBooks() {
+
+  }
+  static addBook(book) {
+
+  }
+  static removeBook(isbn) {
+
+  }
+}
